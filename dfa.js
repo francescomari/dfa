0321b99387ffbe3c59555ca5031ae015555db944
Francesco Mari <mari.francesco@gmail.com>
2013-04-23 02:56:23 +0200

    Code style

--- dfa.js
+++ dfa.js
@@ -1,116 +1,114 @@
 "use strict";
 
-exports.create = function (definition) {
+function validate(definition) {
     var error;
 
-    function validate() {
-        var error;
+    function forEachFinal(f) {
+        var i, result;
 
-        function forEachFinal(f) {
-            var i, result;
+        for (i = 0; i < definition.finals.length; i += 1) {
+            result = f(definition.finals[i]);
 
-            for (i = 0; i < definition.finals.length; i += 1) {
-                result = f(definition.finals[i]);
-
-                if (result) {
-                    return result;
-                }
+            if (result) {
+                return result;
             }
         }
+    }
 
-        function forEachTransition(f) {
-            var state, character, destination, result;
+    function forEachTransition(f) {
+        var state, character, destination, result;
 
-            for (state in definition.states) {
-                if (definition.states.hasOwnProperty(state)) {
-                    for (character in definition.states[state]) {
-                        if (definition.states[state].hasOwnProperty(character)) {
-                            destination = definition.states[state][character];
+        for (state in definition.states) {
+            if (definition.states.hasOwnProperty(state)) {
+                for (character in definition.states[state]) {
+                    if (definition.states[state].hasOwnProperty(character)) {
+                        destination = definition.states[state][character];
 
-                            result = f(state, character, destination);
+                        result = f(state, character, destination);
 
-                            if (result) {
-                                return result;
-                            }
+                        if (result) {
+                            return result;
                         }
                     }
                 }
             }
         }
-
-        function isStateUndefined(name) {
-            return definition.states.hasOwnProperty(name) === false;
-        }
-
-        // Definition is mandatory
-
-        if (!definition) {
-            return "Invalid definition";
-        }
-
-        // Start state is mandatory
-
-        if (!definition.start) {
-            return "Start state not specified";
-        }
-
-        // Final states are mandatory
-
-        if (!definition.finals) {
-            return "Final states not specified";
-        }
-
-        // At least one final state must be specified
-
-        if (definition.finals.length === 0) {
-            return "No final states listed";
-        }
-
-        // Definition of states must be specified
-
-        if (!definition.states) {
-            return "No states specified";
-        }
-
-        // Definition of start state must be specified
-
-        if (isStateUndefined(definition.start)) {
-            return "The initial state has no definition";
-        }
-
-        // Definition of every final state must be specified
-
-        error = forEachFinal(function (f) {
-            if (isStateUndefined(f)) {
-                return "Final state '" + f + "' has no definition";
-            }
-        });
-
-        if (error) {
-            return error;
-        }
-
-        // Each state must process one character at a time and the destination must have a 
-        // definition
-
-        error = forEachTransition(function (state, character, destination) {
-            if (character.length !== 1) {
-                return "Invalid character '" + character + "' for state '" + state + "'";
-            }
-
-            if (isStateUndefined(destination)) {
-                return "State '" + state + "' has a non-existing destination '" + destination + "'";
-            }
-        });
-
-        if (error) {
-            return error;
-        }
-
-        return null;
     }
 
-    error = validate();
+    function isStateUndefined(name) {
+        return definition.states.hasOwnProperty(name) === false;
+    }
+
+    // Definition is mandatory
+
+    if (!definition) {
+        return "Invalid definition";
+    }
+
+    // Start state is mandatory
+
+    if (!definition.start) {
+        return "Start state not specified";
+    }
+
+    // Final states are mandatory
+
+    if (!definition.finals) {
+        return "Final states not specified";
+    }
+
+    // At least one final state must be specified
+
+    if (definition.finals.length === 0) {
+        return "No final states listed";
+    }
+
+    // Definition of states must be specified
+
+    if (!definition.states) {
+        return "No states specified";
+    }
+
+    // Definition of start state must be specified
+
+    if (isStateUndefined(definition.start)) {
+        return "The initial state has no definition";
+    }
+
+    // Definition of every final state must be specified
+
+    error = forEachFinal(function (f) {
+        if (isStateUndefined(f)) {
+            return "Final state '" + f + "' has no definition";
+        }
+    });
+
+    if (error) {
+        return error;
+    }
+
+    // Each state must process one character at a time and the destination must have a 
+    // definition
+
+    error = forEachTransition(function (state, character, destination) {
+        if (character.length !== 1) {
+            return "Invalid character '" + character + "' for state '" + state + "'";
+        }
+
+        if (isStateUndefined(destination)) {
+            return "State '" + state + "' has a non-existing destination '" + destination + "'";
+        }
+    });
+
+    if (error) {
+        return error;
+    }
+}
+
+exports.create = function (definition) {
+    var error;
+
+    error = validate(definition);
 
     if (error) {
         throw new Error(error);
